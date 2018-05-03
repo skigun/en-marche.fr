cb90443c1c5f60c7e97efb892bb1a22e0006039c
Dimitri Gritsajuk <dimitri.gritsajuk@sensiolabs.com>
2018-05-03 11:55:32 +0200

    Change CI config for deploy partial release branch

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,8 +6,7 @@ DOCKER_IMAGE="eu.gcr.io/$GCLOUD_PROJECT/app"
 DOCKER_IMAGE_TAG="$BRANCH-$CIRCLE_SHA1"
 
 # Build the image
-gcloud docker -- pull $DOCKER_IMAGE:$BRANCH
-docker build --cache-from=$DOCKER_IMAGE:$BRANCH -t $DOCKER_IMAGE:$DOCKER_IMAGE_TAG .
+docker build -t $DOCKER_IMAGE:$DOCKER_IMAGE_TAG .
 
 # Push the images to Google Cloud
 gcloud docker -- push $DOCKER_IMAGE:$DOCKER_IMAGE_TAG
